ccbf77bbb7d247fe33c4bf244f6ce66c3482c984
Clyde-fare <clyde.fare@gmail.com>
2014-03-28 13:25:24 +0000

    Initial stats_plugin commit

--- app/plugins/__init__.py
+++ app/plugins/__init__.py
@@ -1,5 +1,5 @@
-import os
 from is_my_code_ready_plugin import is_my_code_ready
+from stats_plugin import stats
 
 def load():
-    return [is_my_code_ready()]
+    return [is_my_code_ready(), stats()]
