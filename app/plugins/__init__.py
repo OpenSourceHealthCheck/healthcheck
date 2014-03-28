a58ece4e1f30cbf57e5a17094c073bbb377f8c26
Mark Basham <mark.basham@diamond.ac.uk>
2014-03-28 13:30:01 +0000

    Added Busfactor

--- app/plugins/__init__.py
+++ app/plugins/__init__.py
@@ -2,6 +2,10 @@ import os
 from is_my_code_ready_plugin import is_my_code_ready
 from license import license
 from readme import readme
+from busfactor import busfactor
 
 def load():
-    return [is_my_code_ready(), license(), readme()]
+    return [is_my_code_ready(), 
+            license(), 
+            readme(),
+            busfactor()]
