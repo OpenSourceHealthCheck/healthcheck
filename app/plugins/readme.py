dfae4b80484d1fe8c85f04ae8c338c0e85707c0a
Robin Wilson <robin@rtwilson.com>
2014-03-28 13:53:55 +0000

    Extended README plugin to allow you to automatically add an issue

--- app/plugins/readme.py
+++ app/plugins/readme.py
@@ -8,11 +8,18 @@ class readme(html_provider_base):
         Checks if a README file exists
         '''
 
+        button = """<form method="POST" action="/create_issue">
+        <input type="hidden" name="repo" value="%s">
+        <input type="hidden" name="user" value="%s">
+        <input type="submit" value="Add an issue"></form>""" % (repo.name, repo.owner)
+
         readme = repo.readme()
 
         if readme is None:
             # No readme!
-            content = "The repository doesn't have a README file. This is essential to make it easy for other people to use your code."
+            content = """The repository doesn't have a README file.
+This is essential to make it easy for other people to use your code. We can automatically add an issue
+to the project to ask the developers to create one.\n""" + button
         else:
             # Yes readme!
             content = "Great - there's a README file here"
