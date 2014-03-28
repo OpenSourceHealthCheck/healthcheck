dac11a17e8085f27b6ab825102c3101ecd8d8de0
Robin Wilson <robin@rtwilson.com>
2014-03-28 08:45:19 +0000

    Added test for github API

--- app/views.py
+++ app/views.py
@@ -1,5 +1,11 @@
 from app import app
 
+from github3 import login
+
+
 @app.route('/')
 def index():
-    return "Hello, CW14 again!"
+	gh = login(token='9829d6638caa5fc1ad576e4e345ca266fd626d8c')
+	repo = gh.repository('robintw', 'Py6S')
+
+    return repo.readme().decoded
