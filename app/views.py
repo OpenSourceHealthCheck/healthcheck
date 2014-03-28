18a60188eead06cf83005fa54e4af74a6f8da05b
Robin Wilson <robin@rtwilson.com>
2014-03-28 11:00:05 +0000

    Added run method stub which will display results on a page.

--- app/views.py
+++ app/views.py
@@ -1,9 +1,16 @@
-from flask import render_template
+from flask import render_template, request, Markup
 from app import app
 from github3 import login
 import os
 
-
 @app.route('/')
 def index():
 	return render_template("index.html")
+
+@app.route('/run', methods=['POST', 'GET'])
+def run():
+	results = ['<div>Some result here</div>',
+			   '<div>Another result here</div>']
+
+	results = map(Markup, results)
+	return render_template("results.html", results=results)
