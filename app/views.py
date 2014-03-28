150254522b3dc5f9df20cc9b574649db2cee16b8
Robin Wilson <robin@rtwilson.com>
2014-03-28 08:46:57 +0000

    Fixed indentation problem

--- app/views.py
+++ app/views.py
@@ -8,4 +8,4 @@ def index():
 	gh = login(token='9829d6638caa5fc1ad576e4e345ca266fd626d8c')
 	repo = gh.repository('robintw', 'Py6S')
 
-    return repo.readme().decoded
+	return repo.readme().decoded
