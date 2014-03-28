cc89447973e87928df68853164e4bdc112b11354
Clyde-fare <clyde.fare@gmail.com>
2014-03-28 13:32:56 +0000

    Initial weekly_commits_plugin commit

--- app/plugins/__init__.py
+++ app/plugins/__init__.py
@@ -1,5 +1,6 @@
 import os
 from is_my_code_ready_plugin import is_my_code_ready
+from weekly_commits_plugin import weekly_commits
 from license import license
 from readme import readme
 from busfactor import busfactor
