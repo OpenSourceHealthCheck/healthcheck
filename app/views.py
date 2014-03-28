fffb47a8121a15d38a42bd8b30ca7274efb0f1ee
Robin Wilson <robin@rtwilson.com>
2014-03-28 11:53:10 +0000

    Rejigged run methods, now everything redirects to /check/username/reponame

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, request, Markup
+from flask import render_template, request, Markup, redirect, url_for
 from app import app
 from github3 import login
 import os
@@ -8,9 +8,7 @@ def index():
 	return render_template("index.html")
 
 @app.route('/run', methods=['POST', 'GET'])
-def run():
-	gh = login(token=os.environ['GITHUB_API_TOKEN'])
-
+def run_from_post():
 	submitted_repo = request.form['repo']
 
 	# if 'http' in submitted_repo:
@@ -36,14 +34,19 @@ def run():
 			reponame = splitted[-1]
 
 		print username, reponame
-		repo = gh.repository(username, reponame)
-		if repo is None:
-			return "Error - Repo doesn't exist!"
 	except:
 		return "Error - Repo doesn't exist!"
-	# else:
+	
+	return redirect(url_for('run_from_url', username=username, reponame=reponame))
+	#return run_from_url(username, reponame)
 
+@app.route('/check/<username>/<reponame>')
+def run_from_url(username, reponame):
+	gh = login(token=os.environ['GITHUB_API_TOKEN'])
 
+	repo = gh.repository(username, reponame)
+	if repo is None:
+		return "Error - Repo doesn't exist!"
 
 
 	results = ['<div><b>Repository:</b> %s</div>' % (repo.name),
