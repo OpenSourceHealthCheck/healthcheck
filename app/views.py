2e0f5caf86c722661e7062f58299d3f9c9a23d9f
Robin Wilson <robin@rtwilson.com>
2014-03-28 10:35:33 +0000

    Use view rather than README files

--- app/views.py
+++ app/views.py
@@ -6,9 +6,4 @@ import os
 
 @app.route('/')
 def index():
-	gh = login(token=os.environ['GITHUB_API_TOKEN'])
-	repo = gh.repository('robintw', 'Py6S')
-
-	#return repo.readme().decoded
-
 	return render_template("index.html")
