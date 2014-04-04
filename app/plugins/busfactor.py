30d48a84f497a99ac18273a0b75437072a53b781
James Spencer <j.spencer@imperial.ac.uk>
2014-04-04 21:21:33 +0100

    Similar bug fix to annual bus factor.

--- app/plugins/busfactor.py
+++ app/plugins/busfactor.py
@@ -22,7 +22,7 @@ class busfactor(html_provider_base):
         content = "The <a href='http://en.wikipedia.org/wiki/Bus_factor'>bus factor</a> for this code is approximately %i over all time<br>" %(busfactor)
         
         comarray = np.array(commits)[0:52]
-        useful = (comarray > comarray.mean())
+        useful = (comarray >= comarray.mean())
         busfactor = useful.sum()
         
         content += "The bus factor in the last year is approximately %i" % (busfactor)
